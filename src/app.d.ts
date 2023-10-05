502b258c7681aa43127522529f25d48acb205e76
brodaclop <biziclop@gmail.com>
2023-10-05 21:30:43 +0100

    cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,13 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Platform {
+			context: {
+				waitUntil(promise: Promise<unknown>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
+
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
@@ -9,4 +16,4 @@ declare global {
 	}
 }
 
-export {};
+export { };
