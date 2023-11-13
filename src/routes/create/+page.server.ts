6a8f82e80ffc7c581c9040d9230efa69a7db2860
brodaclop <biziclop@gmail.com>
2023-11-13 23:59:15 +0000

    i'm close to giving up

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -1,8 +1,10 @@
 import { saveCharacter } from '$lib/server/Db';
-import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { fail, redirect, type Actions, error } from '@sveltejs/kit';
 
 export const actions: Actions = {
     saveCharacter: async ({ request, platform }) => {
+        console.log('seriously, what is going on');
+        throw error(401, 'come on now');
         const formData = await request.formData();
         const characterString = formData.get('character');
         if (characterString) {
