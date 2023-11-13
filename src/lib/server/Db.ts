b767fa40a2cc98bfa3082944daa543489b3370bd
brodaclop <biziclop@gmail.com>
2023-11-13 23:43:01 +0000

    debug save

--- src/lib/server/Db.ts
+++ src/lib/server/Db.ts
@@ -1,5 +1,6 @@
 import type { KVNamespace } from "@cloudflare/workers-types";
 import type { Character, CharacterInfo } from "../../model/Karakter";
+import { fail } from "@sveltejs/kit";
 
 let initialised = false;
 
@@ -40,9 +41,13 @@ export const loadAllCharacters = async (platform: App.Platform): Promise<Array<C
 
 export const saveCharacter = async (platform: App.Platform, char: Character) => {
     ensureInit(platform);
-    const { success } = await platform.env.D1_DB.prepare('insert into Characters (id, user, name, ancestry, background, level, payload) VALUES (?1,?2,?3,?4,?5,?6,?7) ON CONFLICT(id) DO UPDATE SET user=?2, name=?3, ancestry=?4, background=?5, level=?6, payload=?7')
+    const { success, error } = await platform.env.D1_DB.prepare('insert into Characters (id, user, name, ancestry, background, level, payload) VALUES (?1,?2,?3,?4,?5,?6,?7) ON CONFLICT(id) DO UPDATE SET user=?2, name=?3, ancestry=?4, background=?5, level=?6, payload=?7')
         .bind(char.id, 'global', char.name, char.ancestry, char.background, char.levels.length, JSON.stringify(char))
         .run();
+
+    if (!success) {
+        fail(500, error);
+    }
 
     const loadedChar = loadCharacter(platform, char.id);
     console.log('loaded', loadedChar);
