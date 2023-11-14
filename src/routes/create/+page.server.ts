3ae16b61cd8ea75dae9b105875c88d3c9e697ccc
brodaclop <biziclop@gmail.com>
2023-11-14 00:03:48 +0000

    now we're getting somewhere

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -3,13 +3,14 @@ import { fail, redirect, type Actions, error } from '@sveltejs/kit';
 
 export const actions: Actions = {
     saveCharacter: async ({ request, platform }) => {
-        console.log('seriously, what is going on');
-        throw error(401, 'come on now');
         const formData = await request.formData();
         const characterString = formData.get('character');
+        console.log('characterString', characterString);
         if (characterString) {
             const character = JSON.parse(characterString.toString());
+            console.log('character', character);
             saveCharacter(platform!, character);
+            console.log('saved character');
             throw redirect(303, `/character/${character.id}`);
         } else {
             fail(400);
