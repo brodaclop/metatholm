bf27a5d4354d72bb8668c46b13270d4c1846d63e
brodaclop <biziclop@gmail.com>
2024-01-23 01:27:31 +0000

    small fix

--- generateLoreList.js
+++ generateLoreList.js
@@ -39,16 +39,6 @@ Object.entries(diff).forEach(([key, langsPresent]) => {
     }
 });
 
-langs.en.forEach(f => {
-    const key = `${f.category}:${f.name}`;
-    if (diff[key]) {
-        diff.key.push('en');
-    } else {
-        diff.key = ['hu'];
-    }
-});
-
-
 const langDefs = Object.fromEntries(Object.entries(langs).map(([lang, files]) => {
     files.sort((a, z) => {
         let ret = a.category.localeCompare(z.category);
