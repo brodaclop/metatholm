091eac42efb9c9020e127f7ef02ed58531264098
brodaclop <biziclop@gmail.com>
2024-02-04 23:47:38 +0000

    wtaf

--- src/lib/server/Db.ts
+++ src/lib/server/Db.ts
@@ -8,33 +8,33 @@ let initialised = false;
 
 //TODO: use virtual generated columns for name, ancestry, background and level
 
-const ensureInit = async (platform: App.Platform) => {
-    if (!initialised) {
-        await platform.env.D1_DB.exec(
-            'CREATE TABLE IF NOT EXISTS Characters (' +
-            'id string primary key,' +
-            'user string not null,' +
-            'name string not null,' +
-            'ancestry string not null,' +
-            'background string not null,' +
-            'level integer not null,' +
-            'payload text not null' +
-            ')'
-        );
-        //await platform.env.D1_DB.exec('drop table if exists CharacterArchive');
-        await platform.env.D1_DB.exec(
-            'CREATE TABLE IF NOT EXISTS CharacterArchive (' +
-            'key integer primary key autoincrement, ' +
-            'id string not null,' +
-            'user string not null,' +
-            'timestamp integer not null,' +
-            'payload text not null' +
-            ')'
-        );
+const ensureInit = async (platform: App.Platform): Promise<string> => {
+    try {
+        if (!initialised) {
+            const res1 = await platform.env.D1_DB.exec(
+                'CREATE TABLE IF NOT EXISTS Characters (' +
+                'id string primary key,' +
+                'user string not null,' +
+                'name string not null,' +
+                'ancestry string not null,' +
+                'background string not null,' +
+                'level integer not null,' +
+                'payload text not null' +
+                ')'
+            );
+            //await platform.env.D1_DB.exec('drop table if exists CharacterArchive');
+            const res2 = await platform.env.D1_DB.exec(
+                'CREATE TABLE IF NOT EXISTS CharacterArchive (' +
+                'key integer primary key autoincrement, ' +
+                'id string not null,' +
+                'user string not null,' +
+                'timestamp integer not null,' +
+                'payload text not null' +
+                ')'
+            );
 
-        try {
             //await platform.env.D1_DB.exec('drop table if exists NPC');
-            const res = await platform.env.D1_DB.exec(
+            const res3 = await platform.env.D1_DB.exec(
                 'CREATE TABLE IF NOT EXISTS NPC (' +
                 'id string primary key,' +
                 'user string not null,' +
@@ -42,28 +42,29 @@ const ensureInit = async (platform: App.Platform) => {
                 'name as (json_extract(payload, \'$.name\'))' +
                 ')'
             );
-            console.info('execution result', res);
-            throw new Error('creating NPC table: ' + JSON.stringify(res));
-        } catch (e) {
-            console.error('failed to create NPC table', e);
+
+            const res4 = await platform.env.D1_DB.exec(
+                'CREATE TABLE IF NOT EXISTS Encounters (' +
+                'id string primary key,' +
+                'user string not null,' +
+                'payload text not null,' +
+                'name as (json_extract(payload, \'$.name\'))' +
+                ')'
+            );
+
+
+            initialised = true;
+
+            return 'Table creation: ' + [res1, res2, res3, res4].map(r => JSON.stringify(r)).join('\n');
         }
-
-        await platform.env.D1_DB.exec(
-            'CREATE TABLE IF NOT EXISTS Encounters (' +
-            'id string primary key,' +
-            'user string not null,' +
-            'payload text not null,' +
-            'name as (json_extract(payload, \'$.name\'))' +
-            ')'
-        );
-
-
-        initialised = true;
+        return 'Already initialised'
+    } catch (e) {
+        return 'failed to initialise db: ' + JSON.stringify(e);
     }
 }
 
 export const listCharacters = async (platform: App.Platform): Promise<Array<CharacterInfo>> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select id, name, background, ancestry, level from Characters');
     return (await stmt.all()).results.map(({ id, name, background, ancestry, level }) => ({
         id,
@@ -75,13 +76,13 @@ export const listCharacters = async (platform: App.Platform): Promise<Array<Char
 }
 
 export const loadAllCharacters = async (platform: App.Platform): Promise<Array<Character>> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload from Characters');
     return (await stmt.all()).results.map(({ payload }) => JSON.parse(payload as string));
 }
 
 const archiveCharacter = async (platform: App.Platform, id: string) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     let char: Character | undefined = undefined;
     try {
         char = await loadCharacter(platform, id);
@@ -99,13 +100,13 @@ const archiveCharacter = async (platform: App.Platform, id: string) => {
 }
 
 export const loadArchiveVersions = async (platform: App.Platform, id: string): Promise<Array<{ char: Character, timestamp: number }>> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload, timestamp from CharacterArchive where id=? order by key desc').bind(id);
     return (await stmt.all()).results.map(({ payload, timestamp }) => ({ char: JSON.parse(payload as string), timestamp: timestamp as number }));
 }
 
 export const saveCharacter = async (platform: App.Platform, char: Character) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     await archiveCharacter(platform, char.id);
     const res = await platform.env.D1_DB.prepare('insert into Characters (id, user, name, ancestry, background, level, payload) VALUES (?1,?2,?3,?4,?5,?6,?7) ON CONFLICT(id) DO UPDATE SET user=?2, name=?3, ancestry=?4, background=?5, level=?6, payload=?7')
         .bind(char.id, 'global', char.name, char.ancestry, char.background, char.levels.length, JSON.stringify(char))
@@ -113,14 +114,14 @@ export const saveCharacter = async (platform: App.Platform, char: Character) => 
 }
 
 export const deleteCharacter = async (platform: App.Platform, char: Pick<Character, 'id'>) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const { success } = await platform.env.D1_DB.prepare('delete from Characters where id = ?')
         .bind(char.id)
         .run();
 }
 
 export const loadCharacter = async (platform: App.Platform, id: string): Promise<Character> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload from Characters where id = ?').bind(id);
     const found = await stmt.first();
     if (!found) {
@@ -130,14 +131,14 @@ export const loadCharacter = async (platform: App.Platform, id: string): Promise
 }
 
 export const saveNPC = async (platform: App.Platform, char: NPC) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const res = await platform.env.D1_DB.prepare('insert into NPC (id, user, payload) VALUES (?1,?2,?3) ON CONFLICT(id) DO UPDATE SET user=?2, payload=?3')
         .bind(char.id, 'global', JSON.stringify(char))
         .run();
 }
 
 export const listNPCs = async (platform: App.Platform): Promise<Array<Pick<NPC, 'id' | 'name'>>> => {
-    ensureInit(platform);
+    const res = await ensureInit(platform);
     try {
         const stmt = platform.env.D1_DB.prepare('select id, name from NPC');
         return (await stmt.all()).results.map(({ id, name }) => ({
@@ -145,20 +146,20 @@ export const listNPCs = async (platform: App.Platform): Promise<Array<Pick<NPC, 
             name,
         } as Pick<NPC, 'id' | 'name'>));
     } catch (e) {
-        throw new Error('my custom error');
+        throw new Error('my custom error: ' + res);
     }
 
 }
 
 export const deleteNPC = async (platform: App.Platform, char: Pick<NPC, 'id'>) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const { success } = await platform.env.D1_DB.prepare('delete from NPC where id = ?')
         .bind(char.id)
         .run();
 }
 
 export const loadNPC = async (platform: App.Platform, id: string): Promise<NPC> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload from NPC where id = ?').bind(id);
     const found = await stmt.first();
     if (!found) {
@@ -168,20 +169,20 @@ export const loadNPC = async (platform: App.Platform, id: string): Promise<NPC> 
 }
 
 export const loadAllNPCs = async (platform: App.Platform): Promise<Array<NPC>> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload from NPC');
     return (await stmt.all()).results.map(({ payload }) => JSON.parse(payload as string));
 }
 
 export const saveEncounter = async (platform: App.Platform, enc: Encounter) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const res = await platform.env.D1_DB.prepare('insert into Encounters (id, user, payload) VALUES (?1,?2,?3) ON CONFLICT(id) DO UPDATE SET user=?2, payload=?3')
         .bind(enc.id, 'global', JSON.stringify(enc))
         .run();
 }
 
 export const listEncounters = async (platform: App.Platform): Promise<Array<Pick<Encounter, 'id' | 'name'>>> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select id, name from Encounters');
     return (await stmt.all()).results.map(({ id, name }) => ({
         id,
@@ -190,14 +191,14 @@ export const listEncounters = async (platform: App.Platform): Promise<Array<Pick
 }
 
 export const deleteEncounter = async (platform: App.Platform, char: Pick<Encounter, 'id'>) => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const { success } = await platform.env.D1_DB.prepare('delete from Encounters where id = ?')
         .bind(char.id)
         .run();
 }
 
 export const loadEncounter = async (platform: App.Platform, id: string): Promise<Encounter> => {
-    ensureInit(platform);
+    await ensureInit(platform);
     const stmt = platform.env.D1_DB.prepare('select payload from Encounters where id = ?').bind(id);
     const found = await stmt.first();
     if (!found) {
