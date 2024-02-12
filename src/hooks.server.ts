6ad29ea771c4037cef1466df0eecf1f71b0d665c
brodaclop <biziclop@gmail.com>
2024-02-12 19:56:11 +0000

    oh ffs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,18 +12,7 @@ declare module "lucia" {
     }
 }
 
-const { Miniflare, Log, LogLevel } = await import("miniflare");
 
-const mf = dev ? new Miniflare({
-    log: new Log(LogLevel.INFO),
-    // kvPersist: "./kv-data",
-    // kvNamespaces: ["KV"],
-    d1Persist: "./d1-data",
-    d1Databases: ["D1_DB"],
-    // you should also be able to add durable objects & r2
-    script: "",
-    modules: true,
-}) : undefined;
 
 export let lucia: Lucia<Record<never, never>, {
     username: string;
@@ -32,66 +21,79 @@ export let lucia: Lucia<Record<never, never>, {
 }> | undefined = undefined;
 
 export async function handle({ event, resolve }) {
-    if (dev && mf) {
+    if (dev) {
+        const { Miniflare, Log, LogLevel } = await import("miniflare");
+
+        const mf = new Miniflare({
+            log: new Log(LogLevel.INFO),
+            // kvPersist: "./kv-data",
+            // kvNamespaces: ["KV"],
+            d1Persist: "./d1-data",
+            d1Databases: ["D1_DB"],
+            // you should also be able to add durable objects & r2
+            script: "",
+            modules: true,
+        })
         event.platform = { env: await mf.getBindings() };
     }
+    if (event.platform) {
+        if (!lucia) {
+            console.debug('Initialising lucia');
+            const db = event.platform!.env.D1_DB;
+            await ensureInit(db);
+            const adapter = new D1Adapter(db, {
+                user: "user",
+                session: "session"
+            });
 
-    if (!lucia) {
-        console.debug('Initialising lucia');
-        const db = event.platform!.env.D1_DB;
-        await ensureInit(db);
-        const adapter = new D1Adapter(db, {
-            user: "user",
-            session: "session"
-        });
-
-        lucia = new Lucia(adapter, {
-            sessionCookie: {
-                attributes: {
-                    secure: !dev
+            lucia = new Lucia(adapter, {
+                sessionCookie: {
+                    attributes: {
+                        secure: !dev
+                    }
+                },
+                getUserAttributes: (attributes) => {
+                    return {
+                        username: attributes.username,
+                        githubId: attributes.github_id,
+                        googleId: attributes.google_id,
+                    };
                 }
-            },
-            getUserAttributes: (attributes) => {
-                return {
-                    username: attributes.username,
-                    githubId: attributes.github_id,
-                    googleId: attributes.google_id,
-                };
+            });
+        }
+
+        event.platform!.env.lucia = lucia;
+
+        const sessionId = event.cookies.get(lucia.sessionCookieName);
+        if (!sessionId) {
+            event.locals.user = null;
+            event.locals.session = null;
+        } else {
+            const { session, user } = await lucia.validateSession(sessionId);
+            if (session?.fresh) {
+                const sessionCookie = lucia.createSessionCookie(session.id);
+                event.cookies.set(sessionCookie.name, sessionCookie.value, {
+                    path: ".",
+                    ...sessionCookie.attributes
+                });
             }
-        });
-    }
-
-    event.platform!.env.lucia = lucia;
-
-    const sessionId = event.cookies.get(lucia.sessionCookieName);
-    if (!sessionId) {
-        event.locals.user = null;
-        event.locals.session = null;
-    } else {
-        const { session, user } = await lucia.validateSession(sessionId);
-        if (session?.fresh) {
-            const sessionCookie = lucia.createSessionCookie(session.id);
-            event.cookies.set(sessionCookie.name, sessionCookie.value, {
-                path: ".",
-                ...sessionCookie.attributes
-            });
+            if (!session) {
+                const sessionCookie = lucia.createBlankSessionCookie();
+                event.cookies.set(sessionCookie.name, sessionCookie.value, {
+                    path: ".",
+                    ...sessionCookie.attributes
+                });
+            }
+            event.locals.user = user as never;
+            event.locals.session = session;
         }
-        if (!session) {
-            const sessionCookie = lucia.createBlankSessionCookie();
-            event.cookies.set(sessionCookie.name, sessionCookie.value, {
-                path: ".",
-                ...sessionCookie.attributes
-            });
-        }
-        event.locals.user = user as never;
-        event.locals.session = session;
-    }
 
-    // check is authenticated endpoint
-    const needsAuth = event.url.pathname !== '/' && !event.url.pathname.startsWith('/login') && !event.url.pathname.startsWith('/logout');
-    console.log('path', event.url.pathname, 'needsAuth', needsAuth);
-    if (!event.locals.user && needsAuth) {
-        throw redirect(302, "/");
+        // check is authenticated endpoint
+        const needsAuth = event.url.pathname !== '/' && !event.url.pathname.startsWith('/login') && !event.url.pathname.startsWith('/logout');
+        console.log('path', event.url.pathname, 'needsAuth', needsAuth);
+        if (!event.locals.user && needsAuth) {
+            throw redirect(302, "/");
+        }
     }
 
     return resolve(event);
