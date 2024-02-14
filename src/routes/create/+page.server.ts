24ab12578f4bf593688c566826019e24ccfe9c48
brodaclop <biziclop@gmail.com>
2024-02-14 18:10:31 +0000

    closingin

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -5,12 +5,12 @@ export const actions: Actions = {
     saveCharacter: async ({ request, platform, locals }) => {
         const formData = await request.formData();
         const characterString = formData.get('character');
-        // if (characterString) {
-        //     const character = JSON.parse(characterString.toString());
-        //     await saveCharacter(platform!, character, locals.user!.id);
-        //     throw redirect(303, `/character/${character.id}`);
-        // } else {
-        //     throw fail(400);
-        // }
+        if (characterString) {
+            const character = JSON.parse(characterString.toString());
+            //await saveCharacter(platform!, character, locals.user!.id);
+            throw redirect(303, `/character/${character.id}`);
+        } else {
+            throw fail(400);
+        }
     }
 }
